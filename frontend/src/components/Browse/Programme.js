16583e0a1f5e428f0e09472a6907397b95f2b6e5
Lisa Dannegger <lisa.dannegger@hotmail.de>
2021-04-27 16:04:29 +0200

    Routing / Titel Adjustments

--- frontend/src/components/Browse/Programme.js
+++ frontend/src/components/Browse/Programme.js
@@ -1,31 +1,56 @@
-import react from "react";
+import React from "react";
 import styled, { css } from "styled-components";
 import IconHeart from "../../images/Icon-Heart.svg";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
+  padding: 2rem;
   width: 37.5rem;
   height: 21.5rem;
   border-radius: 0.5rem;
   background-color: ${(props) => props.theme.colors.beige};
   margin-bottom: 2rem;
-  display: flex;
   font-size: ${(props) => props.theme.fontSize.h2};
+`;
+
+const ProgrammLink = styled.a`
+  display: flex;
   justify-content: center;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  margin-top: 15%;
 `;
 
 const Icon = styled.img`
-  align-self: flex-start;
+  text-align: left;
+`;
+
+const Hinweis = styled.p`
+  margin: 0;
+  width: 40vw;
+  height: 2.18vh;
+  text-align: right;
+  font-size: ${(props) => props.theme.text.medium};
+`;
+
+const ProgrammHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
 `;
 
 const Programm = (props) => {
   return (
     <Container>
-      <div>
+      <ProgrammHeader>
         <Icon src={IconHeart} alt="Icon Heart" />
-      </div>
-      <h2>{props.title}</h2>
+        <Hinweis>Neu</Hinweis>
+      </ProgrammHeader>
+
+      <ProgrammLink>
+        <Link to="/programm">
+          <h2>{props.title}</h2>
+        </Link>
+      </ProgrammLink>
     </Container>
   );
 };
